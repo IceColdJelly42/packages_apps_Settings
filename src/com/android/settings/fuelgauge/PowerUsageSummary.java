368920acb80b10625d0d306a3053fde4b82c15d1
LorD ClockaN <davor@losinj.com>
2013-01-23 20:09:24 +0100

    Revert "PowerUsage: show more apps with a more accurate percentage"
    This reverts commit adfaa66d481846b431aefa70cc84a533d597043f.

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -59,7 +59,6 @@ import com.android.settings.users.UserUtils;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -100,7 +99,7 @@ public class PowerUsageSummary extends PreferenceFragment implements Runnable {
     private int mStatsType = BatteryStats.STATS_SINCE_CHARGED;
 
     private static final int MIN_POWER_THRESHOLD = 5;
-    private static final int MAX_ITEMS_TO_LIST = 20;
+    private static final int MAX_ITEMS_TO_LIST = 10;
 
     private long mStatsPeriod = 0;
     private double mMaxPower = 1;
@@ -198,8 +197,8 @@ public class PowerUsageSummary extends PreferenceFragment implements Runnable {
         BatterySipper sipper = pgp.getInfo();
         Bundle args = new Bundle();
         args.putString(PowerUsageDetail.EXTRA_TITLE, sipper.name);
-        args.putString(PowerUsageDetail.EXTRA_PERCENT,
-                new DecimalFormat("@#").format((sipper.getSortValue() * 100 / mTotalPower)));
+        args.putInt(PowerUsageDetail.EXTRA_PERCENT, (int)
+                Math.ceil(sipper.getSortValue() * 100 / mTotalPower));
         args.putInt(PowerUsageDetail.EXTRA_GAUGE, (int)
                 Math.ceil(sipper.getSortValue() * 100 / mMaxPower));
         args.putLong(PowerUsageDetail.EXTRA_USAGE_DURATION, mStatsPeriod);
@@ -409,7 +408,7 @@ public class PowerUsageSummary extends PreferenceFragment implements Runnable {
         for (BatterySipper sipper : mUsageList) {
             if (sipper.getSortValue() < MIN_POWER_THRESHOLD) continue;
             final double percentOfTotal =  ((sipper.getSortValue() / mTotalPower) * 100);
-            if (percentOfTotal < 0.1) continue;
+            if (percentOfTotal < 1) continue;
             PowerGaugePreference pref = new PowerGaugePreference(getActivity(), sipper.getIcon(), sipper);
             final double percentOfMax = (sipper.getSortValue() * 100) / mMaxPower;
             sipper.percent = percentOfTotal;
